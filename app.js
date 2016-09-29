5191cce34dc41737659ff1278821378b1cceda5c
Kellator <k.macleod21@gmail.com>
2016-09-29 18:14:19 -0400

    questions added in js file

--- app.js
+++ app.js
@@ -13,8 +13,47 @@ var state = {
 		choices: ["Thomas", "Karrin", "Mister", "Mouse"],
 		correctInputIndex: 2
 	},
+	{
+		text: "According to Harry Dresden in the novel 'Dead Beat'; what will never die?",
+		choices: ["Polka", "Butters", "Evil", "Magic"],
+		correctInputIndex: 0
+	},
+	{
+		text: "In 'Summer Knight', what injury does Karrin Murphy sustain during the fight at the 24 hour Walmart?",
+		choices: ["Gunshot wound by sniper", "Burned by faerie fire", "Knee injury due to slipping on marbles", "Concussion"],
+		correctInputIndex: 2
+	},
+	{
+		text: "What stuffed animal does Harry Dresden use to subdue the loup garou when it goes on a rampage in the police station?",
+		choices: ["Snoopy", "Garfield", "Teddy Ruxpin", "Hobbs"],
+		correctInputIndex: 0
+	},
+	{
+		text: "In the Dresden Files, whose headquarters are located underneath Edinborough, Scotland?",
+		choices: ["The Winter Court", "The Council of Seven", "The White Council", "The Red Court"],
+		correctInputIndex: 2
+	},
+	{
+		text: "After the events of 'Summer Knight', to which court does Fix belong?",
+		choices:  ["The Winter Court", "The Black Court", "The White Court", "The Summer Court"],
+		correctInputIndex: 3
+	},
+	{
+		text: "Who is Thomas Wraith?",
+		choices: ["The White King", "Karrin Murphy's second husband", "Harry's half-brother", "Harry's favorite bartender"],
+		correctInputIndex: 2
+	},
+	{
+		text: "Where do the signatories of the Unseelie Accords risk the wrath of Mab, Queen of Air and Darkness, if they start fighting with one another?",
+		choices: ["The Archive", "Accorded Neutral Ground", "Within sight of the sea", "Demonreach"],
+		correctInputIndex: 1
+	},
+	{
+		text: "Who is the Chicago mobster with the money colored eyes?",
+		choices: ["John Marcone", "Harley McFinn", "Jared Kincaid", "Lex Hamilton"],
+		correctInputIndex: 0
 	}
-
+	
 	]
 }
 
