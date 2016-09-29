83d8799f6943ba8d2d940a9bb867ede3f1dffcbd
Kellator <k.macleod21@gmail.com>
2016-09-29 15:17:10 -0400

    started adding questions to js in questions object

---
+++ app.js
@@ -0,0 +1,25 @@
+//state object
+var state = {
+	//questions is an object that stores the multiple question values 
+	//text, choices, and correctInputIndex.
+	questions = [ 
+	{ 
+		text: "What is the name of the dinosaur that Harry Dresden reanimates in the novel, 'Dead Beat'?",
+		choices: ["Lucy", "Sue", "Nessie", "Rexy"],
+		correctInputIndex: 1
+	},
+	{
+		text: "Who shoulder-checks Harry Dresden almost every time he returns to his apartment?",
+		choices: ["Thomas", "Karrin", "Mister", "Mouse"],
+		correctInputIndex: 2
+	},
+	}
+
+	]
+}
+
+//functions that modify the state object
+
+//functions that render the state object
+
+//event listeners
